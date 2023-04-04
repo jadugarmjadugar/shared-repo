614b159898824b88f34c2adf10da1ba00552d4bc
jadugarmjadugar <86535184+jadugarmjadugar@users.noreply.github.com>
2023-04-05 00:47:15 +0530

    Update loadscript.groovy

--- vars/loadscript.groovy
+++ vars/loadscript.groovy
@@ -1,5 +1,5 @@
 def call(Map config = [:]) { 
-  def scriptcontents = libraryResource "com/planetpope/scripts/linux/${config.name}"    
+  def scriptcontents = libraryResource "${config.name}"    
   writeFile file: "${config.name}", text: scriptcontents 
   sh "chmod a+x ./${config.name}"
 } 
